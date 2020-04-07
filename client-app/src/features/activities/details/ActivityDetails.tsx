5ed2d67d6b2bdfde4f5baca8983d2e8aa037ff2a
Utpal K Ghosh <oshnicgo@gmail.com>
2020-04-07 14:09:33 +0530

    End of Section 10

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -14,7 +14,8 @@ interface DetailParams {
 }
 
 const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({
-  match
+  match,
+  history
 }) => {
   const activityStore = useContext(ActivityStore);
   const {
@@ -24,10 +25,12 @@ const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({
   } = activityStore;
 
   useEffect(() => {
-    loadActivity(match.params.id);
+    loadActivity(match.params.id)
   }, [loadActivity, match.params.id]);
 
-  if (loadingInitial || !activity) return <LoadingComponent content='Loading activity ...' />
+  if (loadingInitial) return <LoadingComponent content='Loading activity ...' />
+
+  if (!activity) return <h2>Activity not found</h2>
 
   return (
     <Grid>
